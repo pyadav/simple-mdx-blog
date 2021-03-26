72a896be53de3aabe03f16e96a6febd4138f0057
Praveen Yadav <pyadav9678@gmail.com>
2021-03-26 12:20:18 +0530

    chore: added theme provider

---
+++ src/themes/theme.js
@@ -0,0 +1,37 @@
+const THEME = {
+  fonts: {
+    main: "Muli, sans-serif",
+    code: "Roboto Mono, monospace",
+  },
+  colors: {
+    dark: {
+      main: "hsl(207, 70%, 59%)",
+    },
+    light: {
+      main: "hsl(207, 70%, 94%)",
+    },
+  },
+  breakpoints: {
+    mobile: "only screen and (max-width: 50rem)",
+    tablet: "only screen and (max-width: 65rem)",
+  },
+
+  spacing: {
+    xxSmall: ".25rem",
+    xSmall: ".5rem",
+    small: "1rem",
+    medium: "2rem",
+    large: "3rem",
+  },
+
+  animation: {
+    button: "box-shadow 0.3 ease",
+    link: "colors 0.2 ease",
+  },
+
+  shadow: {
+    base: "0px 5px 20px   rgba(30,30, 31, 0.05)",
+  },
+}
+
+export default THEME
